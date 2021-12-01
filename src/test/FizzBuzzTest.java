20b9e584d4db1209c196d0fcab1a99e8a2ea9adf
Francisco Viejobueno <francisco.v@thoughtworks.com>
2021-12-01 12:59:53 +0100

    Marco & Paco | factoria tdd

---
+++ src/test/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+    @Test
+    void should() {
+
+        Assertions.assertTrue(true);
+    }
+}

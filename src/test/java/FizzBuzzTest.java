aabb82543caa42a52dc76c342266778110f56f0e
Francisco Viejobueno <francisco.v@thoughtworks.com>
2021-12-01 13:28:11 +0100

    Marco and Paco | Setup

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testSomething(){
+        Assertions.assertTrue(false);
+    }
+}
